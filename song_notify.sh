e09adb613b08f477efd6c9782e5dd275f621a82d
Zachary Silver <zmansilver@gmail.com>
2021-09-26 21:44:18 -0700

    fixed song_notify so that it only uses spotifyd metadata

--- song_notify.sh
+++ song_notify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 artwork_file="/home/zack/.config/spotifyd/cache/album_artwork"
-artwork_url=$(playerctl metadata | grep artUrl | awk '{print $3}')
-song=$(playerctl metadata --format "{{ artist }} - {{ title }}")
-album=$(playerctl metadata --format "{{ album }}")
+artwork_url=$(playerctl --player=spotifyd metadata | grep artUrl | awk '{print $3}')
+song=$(playerctl metadata --player=spotifyd --format "{{ artist }} - {{ title }}")
+album=$(playerctl metadata --player=spotifyd --format "{{ album }}")
 
 # download artwork
 curl -wL $artwork_url -o $artwork_file 1> /dev/null 2> /dev/null
